27ab0afb13524d1bf78831b345f64a3c0655775e
hk-arafat <arafath45000@gmail.com>
2021-01-18 19:43:22 +0600

    commit

---
+++ script.js
@@ -0,0 +1,38 @@
+//  ////Mile to kilometer//// //
+
+let mile = 2 ;
+let kilo = 1.60934 ;
+const kilometer = mile * kilo;
+console.log(kilometer)
+
+// /////Birthday age///// //
+
+let yearPresent = 2021 ;
+let yearBirthday = 2002 ;
+const myAge = yearPresent - yearBirthday ;
+console.log(`My age ${myAge}`)
+
+/////Bill & Tips //// /// //
+
+let chips = 10 ;
+let choclate = 5 ;
+let totalChips = 5 ;
+let totalChoclate = 10 ;
+const myBill = chips * totalChips + choclate * totalChoclate ;
+ if (myBill >= 80 && myBill <=200) {
+     discountRate = myBill * 0.02 ;
+ } 
+ else {
+     discountRate = myBill * 0.05 ;
+ }
+ const totalBill = myBill - discountRate;
+ console.log(`My Bill ${myBill} & Discount Rate ${discountRate} Then Total Bill ${totalBill} `)
+
+//   /////My Friends Calculator//// //
+let myFriend1 = "Rokibul" ;
+let myFriend2 = "Fahad" ;
+let myFriend3 = "Joni" ;
+let myFriend4 = "Junaid" ;
+let myFriend5 = "Sagor" ;
+const myFriends = ["Rokibul", "Fahad", "Joni","Junaid","Sagor"]
+console.log(myFriends)
